43976d2ed5d41fb5717036d25e211b3c5cb3204e
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:59:09 -0300

    Add files via upload

---
+++ 92-FuncionSumaDeArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//Crea la suma de un array.
+
+namespace _92_SumaDeArray
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            int[] array = new int[3];
+
+            array[0] = 3;
+            array[1] = 3;
+            array[2] = 3;
+
+
+            int suma = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                suma += array[i];
+            }
+
+            Console.WriteLine("La suma de los valores introducidos en el array es: " + suma);
+        }
+    }
+}
